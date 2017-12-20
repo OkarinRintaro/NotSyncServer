ae184a59fb63d37a7e77bbe2ec3bc2af8fb1f9b6
OkarinRintaro <pavelalfin@gmail.com>
2017-12-20 17:19:46 +0300

    fixed: private members

--- Python/async_server.py
+++ Python/async_server.py
@@ -7,51 +7,51 @@ from threading import Thread, Lock
 
 class ReadSocket(asyncore.dispatcher_with_send):
 
-    def __init__(self, sock, ref_serv):
+    def __init__(self, sock, ref_server):
         asyncore.dispatcher_with_send.__init__(self, sock)
-        self.server = ref_serv
+        self.__server = ref_server
 
     def handle_read(self):
         data = self.recv(8)
         id_array, action = struct.unpack('Ii', data)
-        self.serv.change_array(id_array, action)
+        self.__server.change_array(id_array, action)
 
 
 class AsyncServer(asyncore.dispatcher):
-    array = []
-    mutex = Lock()
+    __mutex = Lock()
 
     def __init__(self, port):
         asyncore.dispatcher.__init__(self)
+        self.__array = []
         self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
         self.set_reuse_addr()
         self.bind(('0.0.0.0', port))
         self.listen(5)
-        self.cores = multiprocessing.cpu_count()
+        self.__cores = multiprocessing.cpu_count()
 
     def handle_accept(self):
         pair = self.accept()
         if pair is not None:
-            sock, addr = pair
-            print('Connect New Client', addr)
-            if self.cores > 0:
+            sock, address = pair
+            print('Connect New Client', address)
+            if self.__cores > 0:
                 Thread(target=asyncore.loop, args=())
-                self.cores -= 1
+                self.__cores -= 1
             ReadSocket(sock, self)
         else:
             print('Connect refused!')
 
     def change_array(self, id_array, action):
-        self.mutex.acquire()
+        self.__mutex.acquire()
         print('ID: ', id_array, ' Action: ', action)
-        if len(self.array) < id_array:
-            self.array += [0 for _ in range(id_array*2)]
+        if len(self.__array) < id_array:
+            self.__array += [0 for _ in range(id_array*2)]
         if action == 1:
-            self.array[id_array] += 1
+            self.__array[id_array] += 1
         if action == -1:
-            self.array[id_array] -= 1
-        print('ID: ', id_array, ' Result: ', self.array[id_array])
-        self.mutex.release()
+            self.__array[id_array] -= 1
+        print('ID: ', id_array, ' Result: ', self.__array[id_array])
+        self.__mutex.release()
 
 
 port_ = int(input('Enter port: '))
